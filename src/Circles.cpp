cf9cb037b5aa92f6034a3bbc0702087235ad1070
Letsgo72 <letsgo72.hl@live.com>
2021-01-04 12:58:55 +0000

    Added random positioning

--- src/Circles.cpp
+++ src/Circles.cpp
@@ -10,9 +10,22 @@
 int i;
 ofColor colour(0, 0, 0);
 
+float circle1X;
+float circle1Y;
+
+float circle2X;
+float circle2Y;
+
 
 void Circles::setup(){
 	i = 0;
+
+	circle1X = ofRandom(100, ofGetWidth() - 100);
+	circle1Y = ofRandom(100, ofGetHeight() - 100);
+
+	circle2X = ofRandom(100, ofGetWidth() - 100);
+	circle2Y = ofRandom(100, ofGetHeight() - 100);
+
 }
 
 void Circles::update(){
@@ -29,5 +42,15 @@ void Circles::update(){
 
 void Circles::draw(){
 	ofSetColor(colour);
-	ofDrawCircle(150, 150, 100);
+	
+	ofDrawCircle(circle1X, circle1Y, 100);
+	ofDrawCircle(circle2X, circle2Y, 100);
+
+	ofNoFill();
+	ofSetColor(0);
+
+	ofDrawCircle(circle1X, circle1Y, 100);
+	ofDrawCircle(circle2X, circle2Y, 100);
+
+	ofFill();
 }
